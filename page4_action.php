ca6caa34e33bbaa9a54a79998b3c01642e7ab7d4
Swarna2001 <46354063+Swarna2001@users.noreply.github.com>
2020-05-15 19:09:02 +0530

    Updated page4_action.php

--- page4_action.php
+++ page4_action.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
+
 $host = "localhost";
 $user = "root";
 $password = '';
-$db_name = "tax";
+$db_name = "taxhelp";
 
 $con = mysqli_connect($host, $user, $password, $db_name);
 if(mysqli_connect_errno())
@@ -11,24 +11,42 @@ if(mysqli_connect_errno())
 	die("Failed to connect with MySQL : ".mysqli_connect_error());
 }
 
+	$pan = $_POST["pan"];
 	$d2 = $_POST["d2"];
-	$d3 = $_POST["d3"];
-	$d4 = $_POST["d4"];
-	$d5 = $_POST["d5"];
 	$d6 = $_POST["d6"];
 	$d7 = $_POST["d7"];
 	$d8 = $_POST["d8"];
 	$d9 = $_POST["d9"];
 	$d10 = $_POST["d10"];
 
-$pan = $_SESSION["pan"];
 
-$r1 = "select Totalinc from ded where PAN = $pan;
-$totalinc = mysqli_query($con,$r1);
+$r1 = "SELECT Totalinc from ded where PAN = '$pan'";
+$result = mysqli_query($con,$r1);
+$totalinc = 0;
+if( $result = mysqli_query($con,$r1) )
+{
+	$row_cnt = mysqli_num_rows($result);
 
-$r2 = "select extract(year from sysdate()) - extract(year from DOB) from geninfo 
-	where PAN = $pan";
-$age = mysqli_query($con,$r2);
+	while($row = mysqli_fetch_assoc($result))
+	{
+		$totalinc = $row["Totalinc"];
+	}
+}
+
+$r2 = "select extract(year from sysdate()) - extract(year from DOB) as age from geninfo 
+	where PAN = '$pan'";
+$result = mysqli_query($con,$r2);
+$age = 0;
+if( $result = mysqli_query($con,$r2) )
+{
+	$row_cnt = mysqli_num_rows($result);
+	
+	while($row = mysqli_fetch_assoc($result))
+	{
+		$age = $row["age"];
+	}
+}
+
 
 if($age < 60){
 	if( $totalinc <= 250000 ){
@@ -84,7 +102,6 @@ $sql="insert into comp values('$pan','$d1','$d2','$d3','$d4','$d5','$d6','$d7','
 	'$TotalTax')";
 
 
-
 if(mysqli_query($con, $sql)){
 	header('location: Page5.html');
 }  
@@ -92,5 +109,5 @@ else{
 	header('location: Page4.html'); 
 } 
 
-	
+mysqli_close($con);	
 ?>
